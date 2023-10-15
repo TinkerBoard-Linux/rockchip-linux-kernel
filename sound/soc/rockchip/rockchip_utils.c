26c3b91e2a0e0c72ded2423bd0533d2ac1484542
Sugar Zhang <sugar.zhang@rock-chips.com>
2023-10-15 17:58:05 +0800

    ASoC: rockchip: Introduce rockchip utils common API
    At the monment, we add utils_get/put_performance for
    DMC-DVFS-SCENE policy.
    
    Ref: commit cae65d78f0f4 ("ASoC: rockchip: i2s-tdm: Add support for FIFO-XRUN detection")
    
    Signed-off-by: Sugar Zhang <sugar.zhang@rock-chips.com>
    Change-Id: I188e5cfff960aa3679db5c3a8ee847d9c7b685ee

---
+++ sound/soc/rockchip/rockchip_utils.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Rockchip Utils API
+ *
+ * Copyright (c) 2023 Rockchip Electronics Co. Ltd.
+ */
+
+#include <dt-bindings/soc/rockchip-system-status.h>
+#include <linux/module.h>
+#include <linux/notifier.h>
+#include <soc/rockchip/rockchip-system-status.h>
+#include <sound/pcm_params.h>
+#include <sound/dmaengine_pcm.h>
+
+void rockchip_utils_get_performance(struct snd_pcm_substream *substream,
+				    struct snd_pcm_hw_params *params,
+				    struct snd_soc_dai *dai)
+{
+	unsigned int rate = params_rate(params);
+	unsigned int channels = params_channels(params);
+
+	might_sleep();
+
+	if (rate < 192000 && channels < 8)
+		return;
+
+	dev_dbg(dai->dev, "%s: stream[%d]: rate: %u, channels: %u\n",
+		__func__, substream->stream, rate, channels);
+
+	rockchip_set_system_status(SYS_STATUS_PERFORMANCE);
+}
+EXPORT_SYMBOL_GPL(rockchip_utils_get_performance);
+
+void rockchip_utils_put_performance(struct snd_pcm_substream *substream,
+				    struct snd_soc_dai *dai)
+{
+	unsigned int rate = substream->runtime->rate;
+	unsigned int channels = substream->runtime->channels;
+
+	might_sleep();
+
+	if (rate < 192000 && channels < 8)
+		return;
+
+	dev_dbg(dai->dev, "%s: stream[%d]: rate: %u, channels: %u\n",
+		__func__, substream->stream, rate, channels);
+
+	rockchip_clear_system_status(SYS_STATUS_PERFORMANCE);
+}
+EXPORT_SYMBOL_GPL(rockchip_utils_put_performance);
+
+MODULE_LICENSE("GPL");
