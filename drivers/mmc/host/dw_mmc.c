d3c25379a61abf0e58c969233c4033bb8b6d8c6b
yi-hsin_hung <yi-hsin_hung@asus.com>
2025-05-20 14:04:28 +0800

    MMC: Enlarge the waiting time of "mci_xfer" and "mci_drto"
    The SDCARD Info:
    manfid=0x74, oemid=0x4A60, prod_name=USDU1, hwrev=0x2
    fwrev=0x0, serial=41733FA4, year=2019, month=5
    
    Change-Id: Iff89c3fa4be71f37cacc22cbaa47013ffc37315e
    Signed-off-by: yi-hsin_hung <yi-hsin_hung@asus.com>

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -2101,7 +2101,7 @@ static void dw_mci_set_drto(struct dw_mci *host)
 	dev_dbg(host->dev, "drto_ms: %u\n", drto_ms);
 
 	/* add a bit spare time */
-	drto_ms += 10;
+	drto_ms += 1000;
 
 	spin_lock_irqsave(&host->irq_lock, irqflags);
 	if (!test_bit(EVENT_DATA_COMPLETE, &host->pending_events))
@@ -2126,7 +2126,7 @@ static void dw_mci_set_xfer_timeout(struct dw_mci *host)
 
 	/* add a bit spare time */
 	if (host->dir_status == DW_MCI_RECV_STATUS)
-		xfer_ms += 100;
+		xfer_ms += 1100;
 	else
 		xfer_ms += 2500;
 	spin_lock_irqsave(&host->irq_lock, irqflags);
